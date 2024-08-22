19be57338eecf12c656d86199377b6eff7da52cc
Mixey <Mixey1001@yandex.ru>
2024-08-22 23:58:06 +0400

    Вторая итерация ФЗ-5 -)

--- test/InMemoryHistoryManagerTest.java
+++ test/InMemoryHistoryManagerTest.java
@@ -1,6 +1,12 @@
+import managers.HistoryManager;
+import managers.Managers;
+import managers.TaskManager;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import tasks.Epic;
+import tasks.Status;
+import tasks.Task;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,15 +21,15 @@ class InMemoryHistoryManagerTest {
     @BeforeEach
     public void beforeEach() {
         manager = Managers.getDefault();
-        historyManager = manager.getHistoryManager();
     }
 
     @Test
     void add_shouldAddTaskInHistory() {
-        Task task = new Task("Task", "Task_description");
-        historyManager.add(task);
+        Task task = new Task("tasks.Task", "Task_description");
+        Task savedTask = manager.addTask(task);
+        manager.getTaskById(savedTask.getId());
 
-        final List<Task> history = historyManager.getHistory();
+        final List<Task> history = manager.getHistoryList();
         assertTrue(history != null && !history.isEmpty(), "История пустая.");
         assertEquals(1, history.size(), "Больше чем 1 элемент в истории");
         assertEquals(task, history.get(0), "История не пустая.");
@@ -31,8 +37,8 @@ class InMemoryHistoryManagerTest {
 
     @Test
     void add_checkHistoryAddingOnTasksGet() {
-        Task task = new Task("Task", "Task_description");
-        Epic epic = new Epic("Epic", "Epic_description");
+        Task task = new Task("tasks.Task", "Task_description");
+        Epic epic = new Epic("tasks.Epic", "Epic_description");
 
         Task savedTask = manager.addTask(task);
         Epic savedEpic = manager.addEpic(epic);
@@ -44,23 +50,31 @@ class InMemoryHistoryManagerTest {
         manager.getTaskById(task.getId());
         manager.getEpicById(epic.getId());
 
-        List<Task> actualList = historyManager.getHistory();
+        List<Task> actualList = manager.getHistoryList();
         Assertions.assertEquals(expectedList, actualList);
     }
-
+    /**
+    /*Тест прорверяет, что измененная и ранее прочитанная задача, после второго прочтения сохранит в истории
+     * состояние первой задачи*/
     @Test
-    public void checkThatOldTaskSavePreviousFields() {
-        Task task = new Task("Task", "Task_description");
+    public void CheckThatOldTaskWontBeRemovedOnAddingNew() {
+        Task task = new Task("tasks.Task", "Task_description");
         Task savedTask = manager.addTask(task);
         manager.getTaskById(savedTask.getId());
         savedTask.setName("Task_2");
+        savedTask.setDescription("Task_2_description");
+        savedTask.setStatus(Status.DONE);
 
         manager.updateTask(savedTask);
         manager.getTaskById(savedTask.getId());
 
-        List<Task> viewHistory = manager.getHistoryManager().getHistory();
+        List<Task> viewHistory = manager.getHistoryList();
         assertEquals(2, viewHistory.size());
-        assertEquals("Task", viewHistory.get(0).getName());
+        assertEquals("tasks.Task", viewHistory.get(0).getName());
+        assertEquals("Task_description", viewHistory.get(0).getDescription());
+        assertEquals(Status.NEW, viewHistory.get(0).getStatus());
         assertEquals("Task_2", viewHistory.get(1).getName());
+        assertEquals("Task_2_description", viewHistory.get(1).getDescription());
+        assertEquals(Status.DONE, viewHistory.get(1).getStatus());
     }
 }
