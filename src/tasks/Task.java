29822aa98a3ce35d72e9ed27388e27562743365f
Mixey <Mixey1001@yandex.ru>
2024-08-22 23:58:06 +0400

    Вторая итерация ФЗ-5 -)

--- src/tasks/Task.java
+++ src/tasks/Task.java
@@ -1,3 +1,5 @@
+package tasks;
+
 import java.util.Objects;
 
 public class Task {
@@ -61,7 +63,7 @@ public class Task {
 
     @Override
     public String toString() {
-        return "Task{" +
+        return "tasks.Task{" +
                 "name='" + name + '\'' +
                 ", description='" + description + '\'' +
                 ", id=" + id +
